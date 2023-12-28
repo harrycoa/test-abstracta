d2965fe7d34762759554d1f9f17c8b7792bc2363
harry coa <rodol_2000@hotmail.com>
2023-12-28 08:27:20 -0500

    feat: problema 1 con los 3 mecanismos

--- problema1.js
+++ problema1.js
@@ -1,11 +1,39 @@
-numeros = [1, 4, 5, 19, 20, 23, 3, 17, 2, 8]
+numeros = [1, 4, 5, 19, 20, 23, 3, 17, 2, 8, 9, 17, 63]
 
-function suma(numeros) {
+// funcion utilizando for
+function suma(array) {
     let suma = 0;
-    for (let num of numeros) {
+    for (let num of array) {
         if (num > 3 && num < 20) {
             suma += num;
         }
     }
     return suma;
 }
+
+// funcion utilizando while
+function sumaConWhile(array) {
+    let suma = 0;
+    let i = 0;
+    while (i < array.length) {
+        if (array[i] > 3 && array[i] < 20) {
+            suma += array[i];
+        }
+        i++;
+    }
+    return suma;
+}
+
+// funcion utilizando recursion
+function sumaConRecursion(array, index = 0) {
+    if (index >= array.length) {
+        return 0;
+    }
+
+    const valorActual = (array[index] > 3 && array[index] < 20) ? array[index] : 0;
+    return valorActual + sumaConRecursion(array, index + 1);
+}
+
+
+
+console.log(suma(numeros));
